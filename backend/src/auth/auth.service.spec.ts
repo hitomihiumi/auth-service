c0c61f7c02def85c98a43713394bd8848dc8c5df
hitomihiumi <amdroidbox19@gmail.com>
2026-01-20 12:22:13 +0200

    test: enhance AuthService and UserService tests with mock dependencies

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: {
+            findByID: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from './user.schema';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getModelToken(User.name),
+          useValue: {
+            new: jest.fn().mockResolvedValue({}),
+            constructor: jest.fn().mockResolvedValue({}),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
